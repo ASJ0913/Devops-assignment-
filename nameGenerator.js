39bf80d36a346287c999556affded0122df7d1be
Ashton Johnson <ashton.johnson@arm.com>
2025-05-15 00:17:16 +0100

    Name generator

--- nameGenerator.js
+++ nameGenerator.js
@@ -1,6 +1,6 @@
 function generateRandomName() {
     const adjectives = ["Secret", "Mighty", "Brave", "Swift", "Clever"];
-    const animals = ["cat", "bear", "penguin", "lion", "clown"];
+    const animals = ["orange", "bear", "penguin", "lion", "clown"];
     const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
     const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
     const randomName = randomAdjective + " " + randomAnimal;
